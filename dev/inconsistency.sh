61fb47719b525eacde50fee43ac7a7a1f85ece44
Francis M. Tyers <ftyers@prompsit.com>
2010-12-23 09:53:46 +0000

    update testvoc

--- dev/inconsistency.sh
+++ dev/inconsistency.sh
@@ -2,7 +2,7 @@ TMPDIR=/tmp
 
 if [[ $1 = "nl-af" ]]; then
 
-lt-expand ../apertium-af-nl.nl.dix | grep -v 'HANG' | grep -v '<cmp>' | grep -v REGEX | grep -v '<prn><enc>' | sed 's/:>:/%/g' | sed 's/:<:/%/g' | sed 's/:/%/g' | cut -f2 -d'%' |  sed 's/^/^/g' | sed 's/$/$ ^.<sent>$/g' | tee $TMPDIR/tmp_testvoc1.txt |
+lt-expand ../apertium-af-nl.nl.dix | grep -v 'HANG' | grep -v '<cmp>' | grep -v REGEX | grep -v '<prn><enc>' | sed 's/:>:/%/g' | grep -v ':<:' | sed 's/:/%/g' | cut -f2 -d'%' |  sed 's/^/^/g' | sed 's/$/$ ^.<sent>$/g' | tee $TMPDIR/tmp_testvoc1.txt |
         apertium-pretransfer|
         apertium-transfer ../apertium-af-nl.nl-af.t1x  ../nl-af.t1x.bin  ../nl-af.autobil.bin |
         apertium-interchunk ../apertium-af-nl.nl-af.t2x  ../nl-af.t2x.bin |
@@ -12,7 +12,7 @@ paste -d _ $TMPDIR/tmp_testvoc1.txt $TMPDIR/tmp_testvoc2.txt $TMPDIR/tmp_testvoc
 
 elif [[ $1 = "af-nl" ]]; then
 
-lt-expand ../apertium-af-nl.af.dix | grep -v 'HANG' | grep -v '<cmp>' | grep -v REGEX | grep -v '<prn><enc>' | sed 's/:>:/%/g' | sed 's/:<:/%/g' | sed 's/:/%/g' | cut -f2 -d'%' |  sed 's/^/^/g' | sed 's/$/$ ^.<sent>$/g' | tee $TMPDIR/tmp_testvoc1.txt |
+lt-expand ../apertium-af-nl.af.dix | grep -v 'HANG' | grep -v '<cmp>' | grep -v REGEX | grep -v '<prn><enc>' | sed 's/:>:/%/g' | grep -v ':<:' | sed 's/:/%/g' | cut -f2 -d'%' |  sed 's/^/^/g' | sed 's/$/$ ^.<sent>$/g' | tee $TMPDIR/tmp_testvoc1.txt |
         apertium-pretransfer|
         apertium-transfer ../apertium-af-nl.af-nl.t1x  ../af-nl.t1x.bin  ../af-nl.autobil.bin |
         apertium-interchunk ../apertium-af-nl.af-nl.t2x  ../af-nl.t2x.bin |
