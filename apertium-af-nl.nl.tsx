761c03d14ef234846b15b08fbc56002bba644b9c
Pim Otte <otte.pim@gmail.com>
2011-01-01 15:44:56 +0000

    .nl.tsx basis

---
+++ apertium-af-nl.nl.tsx
@@ -0,0 +1,89 @@
+﻿<?xml version="1.0" encoding="UTF-8"?>
+<tagger name ="Dutch">
+	<tagset>
+		<def-label name ="NOUNM">
+			<tags-item tags="n.m.*"/>
+		</def-label>
+		<def-label name ="NOUNF">
+			<tags-item tags="n.f.*"/>
+		</def-label>
+		<def-label name ="NOUNMF">
+			<tags-item tags="n.mf.*"/>
+		</def-label>
+		<def-label name ="NOUNNT">
+			<tags-item tags="n.nt.*"/>
+		</def-label>
+		<def-label name ="ADJPRED">
+			<tags-item tags="adj.pred"/>
+		</def-label>
+		<def-label name ="ADJATTR">
+			<tags-item tags="adj.attr"/>
+		</def-label>
+		<def-label name ="ADJ">
+			<tags-item tags="adj.pred"/>
+			<tags-item tags="adj.attr"/>
+			<tags-item tags="adj.part"/>
+		</def-label>
+		<def-label name ="ADJSINTPRED">
+			<tags-item tags="adj.sint.pred"/>
+			<tags-item tags="adj.sint.comp.pred"/>
+			<tags-item tags="adj.sint.sup.pred"/>
+		</def-label>
+		<def-label name ="ADJSINTATTR">
+			<tags-item tags="adj.sint.attr"/>
+			<tags-item tags="adj.sint.comp.attr"/>
+			<tags-item tags="adj.sint.sup.attr"/>
+		</def-label>
+		<def-label name ="ADJSINT">
+			<tags-item tags="adj.sint.*"/>
+		</def-label>
+		<def-label name ="ADV">
+			<tags-item tags="adj.adv"/>
+		</def-label>
+		<def-label name ="VBLEX">
+			<tags-item tags="vblex.imp.*"/>
+			<tags-item tags="vblex.inf"/>
+			<tags-item tags="vblex.past.*"/>
+			<tags-item tags="vblex.pres.*"/>
+			<tags-item tags="vblex.prs.*"/>
+			<tags-item tags="vblex.pprs.*"/>
+			<tags-item tags="vblex.pp"/>
+		</def-label>
+		<def-label name ="VAUX">
+			<tags-item tags="vaux.*"/>
+		</def-label>
+		<def-label name ="VBMOD">
+			<tags-item tags="vbmod.*"/>
+		</def-label>
+		<def-label name ="VBSER" closed="true">
+			<tags-item tags="vbser.*"/>
+		</def-label>
+		<def-label name ="VBHAVER" closed="true">
+			<tags-item tags="vbhaver.*"/>
+		</def-label>
+		<def-label name ="VBSEP">
+			<tags-item tags="vblex.sep.*"/>
+		</def-label>
+		<def-label name ="CNJCOO" closed="true">
+			<tags-item tags="cnjcoo"/>
+		</def-label>
+		<def-label name ="CNJSUB" closed="true">
+			<tags-item tags="cnjsub"/>
+		</def-label>
+		<def-label name ="CNJADV" closed="true">
+			<tags-item tags="cnjadv"/>
+		</def-label>
+		<def-label name ="PRN" closed="true">
+			<tags-item tags="prn.*"/>
+		</def-label>
+		<def-label name="NPANT">
+			<tags-item tags="np.ant.*"/>
+		</def-label>
+		<def-label name="NPTOP"/>
+			<tags-item tags="np.top.*"/>
+		</def-label>
+		<def-label name="PR"/>
+			<tags-item tags="pr"/>
+		</def-label>		
+	</tagset>
+</tagger>
